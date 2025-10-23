f81c161c4afe5af78bd86c1971157c62a3d04d89
NandakishorNaiR <148868095+NandakishorNaiR@users.noreply.github.com>
2025-10-23 12:14:23 +0530

    Update DJANGO_SETTINGS_MODULE path in wsgi.py
    Fixed settings module path for Render deployment

--- knoxyview/knoxyview/wsgi.py
+++ knoxyview/knoxyview/wsgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'knoxyview.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'knoxyview.knoxyview.settings')
+
 
 application = get_wsgi_application()

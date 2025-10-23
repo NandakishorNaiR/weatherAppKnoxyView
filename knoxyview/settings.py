7c1540d5f032425d5ef26d8fef2304ce2b2ed6cd
NandakishorNaiR <148868095+NandakishorNaiR@users.noreply.github.com>
2025-10-23 14:05:43 +0530

    Fix syntax error in ALLOWED_HOSTS assignment

--- knoxyview/settings.py
+++ knoxyview/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Debug and Allowed Hosts
 DEBUG = True
-ALLOWED_HOSTS = [ALLOWED_HOSTS = ["localhost", "127.0.0.1", "weatherappknoxyview1.onrender.com"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "weatherappknoxyview1.onrender.com"]
 
 
 # Static files (CSS, JavaScript, Images)
